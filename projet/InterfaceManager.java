4f68c4d1e4656f2151d5abd50e49b362345d3cfa
Emma Baudoint <62210726+Emma-bau@users.noreply.github.com>
2020-11-24 17:33:08 +0100

    Update InterfaceManager.java

--- projet/InterfaceManager.java
+++ projet/InterfaceManager.java
@@ -10,9 +10,11 @@ public class InterfaceManager extends JFrame implements ActionListener {
 	 */
 	private static final long serialVersionUID = 1L;
 	final JLabel label = new JLabel("Connexion");
+	final static String LOOKANDFEEL = "Motif";
 
 	public InterfaceManager() 
 	{
+		initLookAndFeel();
 		
 		//fenêtre de connexion 
 		 //Make sure we have nice window decorations.
@@ -39,6 +41,49 @@ public class InterfaceManager extends JFrame implements ActionListener {
 		System.out.println("Connexion réussie");
     }
 	
+	//Style 
+	 private static void initLookAndFeel() {
+	        
+	        // Swing allows you to specify which look and feel your program uses--Java,
+	        // GTK+, Windows, and so on as shown below.
+	        String lookAndFeel = null;
+	        
+	        if (LOOKANDFEEL != null) {
+	            if (LOOKANDFEEL.equals("Metal")) {
+	                lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
+	            } else if (LOOKANDFEEL.equals("System")) {
+	                lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+	            } else if (LOOKANDFEEL.equals("Motif")) {
+	                lookAndFeel = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
+	            } else if (LOOKANDFEEL.equals("GTK+")) { //new in 1.4.2
+	                lookAndFeel = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+	            } else {
+	                System.err.println("Unexpected value of LOOKANDFEEL specified: "
+	                        + LOOKANDFEEL);
+	                lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
+	            }
+	            
+	            try {
+	                UIManager.setLookAndFeel(lookAndFeel);
+	            } catch (ClassNotFoundException e) {
+	                System.err.println("Couldn't find class for specified look and feel:"
+	                        + lookAndFeel);
+	                System.err.println("Did you include the L&F library in the class path?");
+	                System.err.println("Using the default look and feel.");
+	            } catch (UnsupportedLookAndFeelException e) {
+	                System.err.println("Can't use the specified look and feel ("
+	                        + lookAndFeel
+	                        + ") on this platform.");
+	                System.err.println("Using the default look and feel.");
+	            } catch (Exception e) {
+	                System.err.println("Couldn't get specified look and feel ("
+	                        + lookAndFeel
+	                        + "), for some reason.");
+	                System.err.println("Using the default look and feel.");
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 	
 	
 	
