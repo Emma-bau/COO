09036239820c52888912926aaf51c40f150cd9ab
Emma Baudoint <62210726+Emma-bau@users.noreply.github.com>
2020-11-24 17:20:23 +0100

    interface

--- projet/Agent.java
+++ projet/Agent.java
@@ -1,4 +1,12 @@
-public class Agent {
+import javax.swing.*;
+
+public class Agent extends JFrame{
+
 	
+	
+	public static void main(String[] args) {
+		
+		InterfaceManager I = new InterfaceManager();
+	}
     
 }
--- projet/InterfaceManager.java
+++ projet/InterfaceManager.java
@@ -1,3 +1,53 @@
-public class InterfaceManager {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class InterfaceManager extends JFrame implements ActionListener {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InterfaceManager() 
+	{
+		
+		//fenêtre de connexion 
+		 //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        
+        //Create and set up the window.
+        JFrame frame = new JFrame("Chat Session");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JLabel label = new JLabel("Chat");
+        frame.getContentPane().add(label);
+        
+        //Display the window.
+        frame.pack();
+        //à dynamiser
+        frame.setSize(1000,800);
+        frame.setVisible(true);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Connexion réussie");
+    }
+	
+	public Component createConnexionButton()
+	{
+		JButton button = new JButton("Connexion");
+        button.setMnemonic(KeyEvent.VK_I);
+        button.addActionListener(this);
+        label.setLabelFor(button);
+	}
+	
+
+	
+	
+	
+	
+    
+    	
 }
